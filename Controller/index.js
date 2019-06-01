7731cb5f567c36d1908dccf60f6207a92492797b
yakamachi <yakamachipl@gmail.com>
2019-06-01 17:03:43 +0200

    New Page
    Testing

---
+++ Controller/index.js
@@ -0,0 +1,50 @@
+var stack = ["main"];
+//setup
+function onload(){
+var sites = document.getElementsByTagName("iframe");
+Array.from(sites).forEach(function(e,i){
+    if (e.id !== "main")
+        e.style.display = "none";
+})
+
+//Array.from(document.getElementsByTagName("div")).forEach(function(A,i){if (i<2 || i>=document.getElementsByTagName("div").length-2) A.style.display="none"});
+
+document.getElementById("root").style.display="block"
+iframeLoaded();
+}
+  function iframeLoaded() {
+      var iFrameID = document.getElementsByTagName('iframe');
+      Array.from(iFrameID).forEach(function(e,i){
+      if(e) {
+            // here you can make the height, I delete it first, then I make it again
+            e.style.height= "";
+            e.style.height = e.contentWindow.document.body.scrollHeight + "px";
+      }
+      })
+  }
+
+
+
+function pushroute(route){
+    stack.pop()
+    stack.push(route);
+    console.log(stack);
+    refreshroute();
+}
+
+function refreshroute(){
+    var sites = document.getElementsByTagName("iframe");
+    Array.from(sites).forEach(function(e,i){
+    if (e.id !== stack[0])
+        e.style.display = "none";
+    else
+        e.style.display = "";
+        e.style.height = e.contentWindow.document.body.scrollHeight + "px";
+})
+}
+
+function backroute(){
+    if(stack.length !== 0)
+        stack.pop();
+    refreshroute();
+}	
