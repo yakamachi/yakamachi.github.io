d7554a2bfa6932a44063eaa308398d2316cb1c7e
Karol Mitek <yakamachipl@gmail.com>
2019-06-05 22:00:12 +0200

    Add files via upload

--- Controller/kont.js
+++ Controller/kont.js
@@ -1,3 +1,13 @@
+window.addEventListener("load",function() {
+    document.getElementById('my-form').addEventListener("submit",function(e) {
+      e.preventDefault(); // before the code
+      /* do what you want with the form */
+  
+      // Should be triggered on form submit
+      alert('hi');
+    });
+  });
+
 function validateMe(){
 
 
