1bc282debb70f24bd9f65d72891888adb23eee0a
Karol Mitek <yakamachipl@gmail.com>
2019-06-05 23:08:55 +0200

    Add files via upload

--- Controller/kont.js
+++ Controller/kont.js
@@ -1,24 +1,27 @@
-window.addEventListener("load",function() {
-    document.getElementById('my-form').addEventListener("submit",function(e) {
-      e.preventDefault(); // before the code
-      /* do what you want with the form */
+window.addEventListener("load", function () {
+    document.getElementById('my-form').addEventListener("submit", function (e) {
+        e.preventDefault(); // before the code
+        /* do what you want with the form */
         a = document.getElementById('first').value
         b = document.getElementById('last').value
         c = document.getElementById('email').value
         d = document.getElementById('phone').value
         e = document.getElementById('message').value
-        
-        contact = new ContactMe(a,b,c,d,e)
+
+        contact = new ContactMe(a, b, c, d, e)
 
         var yup = validateMe(contact)
 
-      // Should be triggered on form submit
-      if(yup === false)
-        alert('hi');
+        // Should be triggered on form submit
+        if (yup === false)
+            alert('Formularz posiada błędy, popraw go proszę');
+        else{
+            alert('Formularz został wysłany, dziękuję!')
+        }
     });
-  });
+});
 
-function validateMe(contact){
+function validateMe(contact) {
 
     a = contact.first
     b = contact.last
@@ -28,14 +31,14 @@ function validateMe(contact){
 
 
     var constraints = {
-        from: {
+        email: {
             email: true
         }
     };
 
-    var errors = validate({from: e},constraints);
+    var errors = validate(c, constraints);
 
-    if(errors)
+    if (errors)
         return false;
     else
         return true;
